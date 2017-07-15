a6c9e8cd15d5ffda9092039357e16c744716dfad
El'dar Ismagilov <eldarismagilov@gmail.com>
2017-07-15 17:43:37 +0500

    Accessing to Form Data

--- forms/template/src/app/app.component.ts
+++ forms/template/src/app/app.component.ts
@@ -9,6 +9,14 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   genders = ['male', 'female'];
+  user ={
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -16,7 +24,12 @@ export class AppComponent {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form);
-    console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.submitted = true;
   }
 }
